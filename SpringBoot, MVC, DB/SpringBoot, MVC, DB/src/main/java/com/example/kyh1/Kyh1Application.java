becde2af022f769a45dc88c1ec6ce31e4b45652f
Insu-Yoon <ingsu814@gmail.com>
2023-11-27 18:19:42 +0900

    Add files via upload

---
+++ SpringBoot, MVC, DB/SpringBoot, MVC, DB/src/main/java/com/example/kyh1/Kyh1Application.java
@@ -0,0 +1,13 @@
+package com.example.kyh1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyh1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kyh1Application.class, args);
+    }
+
+}
