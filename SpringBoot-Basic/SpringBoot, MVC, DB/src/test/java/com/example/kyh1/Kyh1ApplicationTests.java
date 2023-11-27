1e71600791f6c86397751a753675a97a6222cec3
Insu-Yoon <ingsu814@gmail.com>
2023-11-27 18:47:12 +0900

    Add files via upload

---
+++ SpringBoot-Basic/SpringBoot, MVC, DB/src/test/java/com/example/kyh1/Kyh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kyh1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kyh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
